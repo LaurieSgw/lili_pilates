c6b58a89636e1cd2a20275f365a782495ed9115a
Laurie Segalowitch <lausegalo@gmail.com>
2023-04-26 18:23:16 +0200

    dashboard creation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @reviews = Review.all
+    @reviews = Review.all.last(5)
+  end
+
+  def dashboard
+    @bookings = current_user.bookings
   end
 end
