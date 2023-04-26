853c698e9d1ba589de2dc42c3760bf1479c883e1
Laurie Segalowitch <lausegalo@gmail.com>
2023-04-26 18:25:38 +0200

    courses controller done and view in progress

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,13 +1,33 @@
 class CoursesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_course, only: %i[new create destroy]
+
   def index
+    @courses = Course.all
   end
 
   def new
+    @course = Course.new
   end
 
   def create
+    @course = Course.new(course_params)
+    @course.save
+    redirect_to courses_path
   end
 
   def destroy
+    @course.destroy
+    redirect_to courses_path, status: :see_other
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:date, :available_places)
+  end
+
+  def set_course
+    @course = Course.find(params[:id])
   end
 end
