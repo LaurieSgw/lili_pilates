46869c061623c10c82e2cff93702e6c7e25c7066
Laurie Segalowitch <lausegalo@gmail.com>
2023-05-02 19:05:17 +0200

    first seeds creation-temporary seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,83 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning db"
+Course.destroy_all
+User.destroy_all
+Review.destroy_all
+
+puts "Creating users"
+
+user = User.new(
+  email: "test3@gmail.com",
+  password: "azerty",
+  age: 25,
+  last_name: "Sgw",
+  first_name: "Laurie",
+  phone_number: "0690909090",
+  level: "Débutant",
+  health_record: "/"
+)
+user.save!
+
+user = User.new(
+  email: "test4@gmail.com",
+  password: "azerty",
+  age: 25,
+  last_name: "S",
+  first_name: "Lau",
+  phone_number: "0690909090",
+  level: "Débutant",
+  health_record: "/"
+)
+user.save!
+puts "Users created"
+
+puts "Creating reviews"
+5.times do
+  review = Review.new(
+    content: "Supeeeeer",
+    date: Time.now,
+    user_id: User.all.sample.id
+  )
+  review.save!
+end
+
+puts "Reviews ok"
+
+puts "Creating courses"
+course = Course.new(
+  date: Time.now,
+  available_places: 6,
+  trial_course: "No"
+)
+course.save!
+
+course = Course.new(
+  date: Time.now,
+  available_places: 6,
+  trial_course: "No"
+)
+course.save!
+
+course = Course.new(
+  date: Time.now,
+  available_places: 5,
+  trial_course: "No"
+)
+course.save!
+
+course = Course.new(
+  date: Time.now,
+  available_places: 3,
+  trial_course: "Yes"
+)
+course.save!
+course = Course.new(
+  date: Time.now,
+  available_places: 6,
+  trial_course: "No"
+)
+course.save!
+
+puts "Courses ok"
